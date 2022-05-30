4868dfa9f99b7bc21390173981ec1c6c4e94a718
Hector <hectorpose@gmail.com>
2022-05-30 12:34:43 +0000

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        String[] nombres = {"Paco", "Juan", "Manuel", "Antonio"};
+
+        for(String nombre: nombres){
+            System.out.println(nombre);
+
+        }
+
+
+    }
+}
